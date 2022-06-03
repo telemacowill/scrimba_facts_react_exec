497cf9ef562c835c31027a21de3a6fd2c2f65773
telemacowill <aglwill@gmail.com>
2022-06-03 14:23:53 -0300

    Scrimba_reacfacts

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>     
+            <img src="./images/react-icon-small.png"/>
+            <h3>React Facts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
+    )
+}
